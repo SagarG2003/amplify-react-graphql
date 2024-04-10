a23d42761d23a78581e02111083bce2bc6677485
Sagar Guney <113847560+SagarG2003@users.noreply.github.com>
2024-04-11 01:25:34 +0530

    addded api and storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API,Storage } from "aws-amplify";
+import { generateClient } from 'aws-amplify/api';
 import {
+  Image,
   Button,
   Flex,
   Heading,
-  Image,
   Text,
   TextField,
   View,
@@ -17,6 +17,8 @@ import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
+
+const API = generateClient();
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
@@ -57,7 +59,7 @@ const App = ({ signOut }) => {
     fetchNotes();
     event.target.reset();
   }
-  
+
 
   async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
@@ -70,7 +72,6 @@ const App = ({ signOut }) => {
   }
 
   return (
-    
     <View className="App">
       <Heading level={1}>My Notes App</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
@@ -96,30 +97,42 @@ const App = ({ signOut }) => {
           </Button>
         </Flex>
       </View>
+      <View
+        name="image"
+        as="input"
+        type="file"
+        style={{ alignSelf: "end" }}
+      />
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
+      {notes.map((note) => (
+  <Flex
+    key={note.id || note.name}
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <Text as="strong" fontWeight={700}>
+      {note.name}
+    </Text>
+    <Text as="span">{note.description}</Text>
+    {note.image && (
+      <Image
+        src={note.image}
+        alt={`visual aid for ${notes.name}`}
+        style={{ width: 400 }}
+      />
+    )}
+    <Button variation="link" onClick={() => deleteNote(note)}>
+      Delete note
+    </Button>
+  </Flex>
+))}
       </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
-    
+
   );
-  
 };
 
 export default withAuthenticator(App);
